5a0c2a2914b167830285cddebd6c821b96789753
Vinod Pillai <vinodthebest@gmail.com>
2020-09-26 23:02:41 +0530

    springboot-secuirty: Enabled spring security
    - Step 1 Added spring security and enabled using default user and system generated password.

---
+++ src/main/java/com/vinod/springboot/security/SpringbootSecurityApplication.java
@@ -0,0 +1,15 @@
+package com.vinod.springboot.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication
+@EnableWebSecurity
+public class SpringbootSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinod/springboot/security/SpringbootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinod.springboot.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
